49db42df379c848a9d1ccfa2dc27150f14955107
AzureAD\Leonid <leonid1@litevision-eo.com>
2026-02-05 19:55:22 +0200

    no message

--- DATA/creo_xlsx2struct.m
+++ DATA/creo_xlsx2struct.m
@@ -1,4 +1,4 @@
-function [S, matFileName] = creo_xlsx2struct(defaultSimLocation,ignoreNamesList, loadType)
+function [S, matFileName] = creo_xlsx2struct(defaultSimLocation,ignoreNamesList, loadType, toSave)
 % This function extracts all creo simulate graph results in .xlsx forma
 % Inputs:
 % defaultSimLocation - path to be search for .xlsx
@@ -6,20 +6,14 @@ function [S, matFileName] = creo_xlsx2struct(defaultSimLocation,ignoreNamesList,
 % loadType - 'new' - for new load (default)
 %            'load' - for load already saved files
 
-currentDateTime = char(datetime('now', 'Format','yyyy_MM_dd__hh_mm'));
-callerPath = pwd;
-
-switch nargin
-    case 2
-        loadType = 'new';
-    case 1
-        ignoreNamesList = {''};
-        loadType = 'new';
-    case 0
-        error('Not enough input arguments...')
-    otherwise
-        % full arguments input
+arguments
+    defaultSimLocation % no default
+    ignoreNamesList = {''}       %default - an empty array
+    loadType = 'new'             %by default load new data
+    toSave = false               %by default dont save .mat
 end
+
+currentDateTime = char(datetime('now', 'Format','yyyy_MM_dd__hh_mm'));
 
 switch loadType
     case 'new'
@@ -37,13 +31,19 @@ switch loadType
             [~, name, ~] = fileparts(filteredFileNames{i});
             splittedFileName = split(name, '_');
             filedName = splittedFileName{end}; % Get the last character after '_'
-            S.(filedName) = readtable(fullfile(creoResultsLocation, filteredFileNames{i}));
+            opts = detectImportOptions(fullfile(creoResultsLocation, filteredFileNames{i}));
+            opts.VariableNamingRule = 'modify';     
+            S.(filedName) = readtable(fullfile(creoResultsLocation, filteredFileNames{i}), opts);
         end
 
         matFileName_ = split(creoResultsLocation, '\');
         matFileName = [matFileName_{end-1}, '_', currentDateTime, '.mat'];
-        save(matFileName, 'S');
-        sprintf('File %s was saved...', matFileName)
+
+        if toSave
+
+            save(matFileName, 'S');
+            sprintf('File %s was saved...', matFileName)
+        end
     case 'load'
         dirRes = dir('*.mat');
         if isempty(dirRes)
